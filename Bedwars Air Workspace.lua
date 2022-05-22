d2b590f265cb94c4a607a362489b4d8fb804993e
AirPlayzYT <102592363+AirPlayzYT@users.noreply.github.com>
2022-05-22 18:59:34 -0400

    Bedwars Air Workspace

--- Bedwars Air Workspace.lua
+++ Bedwars Air Workspace.lua
@@ -1,13 +1,17 @@
 local Library = loadstring(game:HttpGet("https://pastebin.com/raw/vff1bQ9F"))()
-local Window = Library.CreateLib("Bedwars Air V1", "BloodTheme")
+local Window = Library.CreateLib("Bedwars Air V2", "BloodTheme")
 
 -- Tab
 
 local Tab1 = Window:NewTab("Bedwars")
 local Tab1Section = Tab1:NewSection("Bedwars")
 
-local Tab2 = Window:NewTab("Credits")
-local Tab2Section = Tab2:NewSection("Air")
+local Tab2 = Window:NewTab("Other")
+local Tab2Section = Tab2:NewSection("Other")
+
+local Tab3 = Window:NewTab("Credits")
+local Tab3Section = Tab3:NewSection("Air")
+local Tab3Section = Tab3:NewSection("Who Inspired Me")
 
 -- Buttons
 
@@ -40,7 +44,11 @@ Tab1Section:NewButton("Lightbed", "Loads Lightbed", function()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/AirPlayzYT/Yes/gh-pages/LIGHTBED%20BETA(not%20mine).lua", true))()
 end)
 
-Tab2Section:NewButton("Credits", "Credits", function()
+Tab2Section:NewButton("Rejoin", "Rejoind The Game", function()
+loadstring(game:HttpGet("https://pastebin.com/raw/1gtVMUz3"))()
+end)
+
+Tab3Section:NewButton("Credits", "Credits", function()
 game.StarterGui:SetCore("SendNotification", {
     Title = "Bedwars OP";
     Text = "Made by Air"; -- what the text says (ofc)
@@ -51,5 +59,12 @@ game.StarterGui:SetCore("SendNotification", {
     Title = "Enjoy";
     Text = "Don't Forget to Subscribe to AirScripts"; -- what the text says (ofc)
     Duration = 30;
+})
+
+Tab3Section:NewButton("Who Inspired Me", "Who Inspired Me", function()
+game.StarterGui:SetCore("SendNotification", {
+	Title = "Who Inspired Me";
+	Text = "JN HH Gaming"; -- what the text says
+	Duration = 30;
 })
 end)
