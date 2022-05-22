01cc3754be362ed06390bc19934e71cea9649615
AirPlayzYT <102592363+AirPlayzYT@users.noreply.github.com>
2022-05-21 23:14:42 -0400

    Bedwars Air Workspace.lua
    yes

--- Bedwars Air Workspace.lua
+++ Bedwars Air Workspace.lua
@@ -1,19 +1,16 @@
 local Library = loadstring(game:HttpGet("https://pastebin.com/raw/vff1bQ9F"))()
 local Window = Library.CreateLib("Bedwars Air V1", "BloodTheme")
 
--- Tabs
+-- Tab
 
 local Tab1 = Window:NewTab("Bedwars")
 local Tab1Section = Tab1:NewSection("Bedwars")
 
 local Tab2 = Window:NewTab("Credits")
-local Tab2Section = Tab2:NewSection("Credits")
-
-local Tab3 = Windows:NewTab("Other")
-local Tab3Section = Tab3:NewSection("Other")
+local Tab2Section = Tab2:NewSection("Air")
 
 -- Buttons
-Tab1Section:NewButton("Vape V4", "Loads Vape V4 Script", function()
+Tab1Section:NewButton("Vape V4", "Loads Vape Script", function()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/7GrandDadPGN/VapeV4ForRoblox/main/NewMainScript.lua", true))()
 end)
 
@@ -21,7 +18,7 @@ Tab1Section:NewButton("Keyboard", "Opens Keyboard", function()
 loadstring(game:HttpGet("https://pastebin.com/raw/kC3dAMvt"))()
 end)
 
-Tab1Section:NewButton("Inf Jumps", "Loads My Inf Jumps ", function()
+Tab1Section:NewButton("Inf Jumps", "Loads My Old Script", function()
 local InfiniteJumpEnabled = true
 game:GetService("UserInputService").JumpRequest:connect(function()
 	if InfiniteJumpEnabled then
@@ -38,16 +35,12 @@ Tab2Section:NewButton("Credits", "Credits", function()
 game.StarterGui:SetCore("SendNotification", {
     Title = "Bedwars OP";
     Text = "Made by Air"; -- what the text says (ofc)
-    Duration = 60;
+    Duration = 30;
 })
 wait(1)
 game.StarterGui:SetCore("SendNotification", {
     Title = "Enjoy";
     Text = "Don't Forget to Subscribe to AirScripts"; -- what the text says (ofc)
-    Duration = 60;
+    Duration = 30;
 })
-end)
-
-Tab3Section:NewButton("Rejoin", "Rejoins the Game", function()
-loadstring(game:HttpGet("https://pastebin.com/raw/1gtVMUz3"))()
 end)
