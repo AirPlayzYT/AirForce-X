e9c54aa315e5343cfb8618855ab53bf8bdd47179
AirPlayzYT <102592363+AirPlayzYT@users.noreply.github.com>
2022-05-29 19:57:23 -0400

    AirForceX Script Hub

--- Air Script Hub Workspace.lua
+++ Air Script Hub Workspace.lua
@@ -4,7 +4,7 @@ local w = library:CreateWindow("Air Script Hub") -- Creates the window
 
 local b = w:CreateFolder("Scripts") -- Creates the folder(U will put here your buttons,etc)
 
-b:Label("Yes",{
+b:Label("AirForce-X",{
     TextSize = 25; -- Self Explaining
     TextColor = Color3.fromRGB(255,255,255); -- Self Explaining
     BgColor = Color3.fromRGB(69,69,69); -- Self Explaining
@@ -12,11 +12,11 @@ b:Label("Yes",{
 }) 
 
 b:Button("Bedwars Air", function()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/AirPlayzYT/Yes/main/Bedwars%20Air%20Workspace.lua", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/AirPlayzYT/AirForce-X/main/Sunlight%20Workspace.lua", true))()
 end)
 
 b:Button("Bedwars Sunlight", function()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/AirPlayzYT/Yes/main/Sunlight%20Workspace.lua", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/AirPlayzYT/AirForce-X/main/Sunlight%20Workspace.lua", true))()
 end)
 
 b:DestroyGui()
