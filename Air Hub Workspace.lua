be02a4832096c95925e42683eedf1e4755ed763d
A1rPl4yzYT <102592363+AirPlayzYT@users.noreply.github.com>
2022-07-16 19:15:34 -0400

    Air Hub Workspace.lua

--- Air Hub Workspace.lua
+++ Air Hub Workspace.lua
@@ -1,19 +1,4 @@
-local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
-local Player = game.Players.LocalPlayer
-local Window = OrionLib:MakeWindow({Name = "KeySystem", HidePremium = false, SaveConfig = true, IntroEnabled = false
 
-OrionLib:MakeNotification({
-	Name = "Logged in!",
-	Content = "You are logged in as"..Player.Name..".",
-	Image = "rbxassetid://4483345998",
-	Time = 5
-})
-
-_G.Key = "57837352"
-_G.KeyInput = "string"
-
-function MakeScriptHub()
-   
 local AirWaterMark = Instance.new("ScreenGui") 
  local TextLabel = Instance.new("TextButton") 
  local UIGradient = Instance.new("UIGradient") 
@@ -1619,48 +1604,4 @@ Tab1Section:NewToggle("Sprint", "Automatically on your sprint", function(state)
                  UnbindFromStepped("Sprint") 
                  SprintCont:stopSprinting() 
          end 
- end) 
-end
-
-function CorrectKeyNotification()
-   OrionLib:MakeNotification({
-	Name = "Correct Key!",
-	Content = "You have entered the correct key!",
-	Image = "rbxassetid://4483345998",
-	Time = 5
-})
-end
-
-function IncorrectKeyNotification()
-   OrionLib:MakeNotification({
-	Name = "Incorrect Key!",
-	Content = "You have entered an incorrect key!",
-	Image = "rbxassetid://4483345998",
-	Time = 5
-})
-local Tab = Window:MakeTab({
-	Name = "Key",
-	Icon = "rbxassetid://4483345998",
-	PremiumOnly = false
-})
-    
-Tab:AddTextbox({
-	Name = "Enter Key",
-	Default = "",
-	TextDisappear = true,
-	Callback = function(Value)
-		_G.KeyInput = Value
-	end	  
-})
-      
-Tab:AddButton({
-	Name = "Check Key",
-	Callback = function()
-      		if _G.KeyInput == _G.Key then
-          MakeScriptHub()
-          CorrectKeyNotification()
-          else
-              IncorrectKeyNotification()
-          end
-  	end    
-})
+ end)
